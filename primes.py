ab1540e147509eb49c8377208994d5351084a814
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-09-29 16:55:47 +0100

    Add input checks for variable 'nunber_of_primes'

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"Value {number_of_primes} is invalid. Please choose a number greater than 0")
     list = []
+    number = 2
+    while len(list) < number_of_primes:
+        prime = True
+        for i in range(2, number):
+            if (number % i == 0):
+                prime = False
+        if prime:
+            list.append(number)
+        number = number + 1
     return list
